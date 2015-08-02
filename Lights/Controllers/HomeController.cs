b13d417cef72a1eb8ff0dac3a04c7cc69a8fac7b
Mattias Sämskar <mattias.samskar@gmail.com>
2015-08-02 17:31:02 +0200

    Initial commit

---
+++ Lights/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lights.Controllers
+{
+    public class HomeController : Controller
+    {
+        static readonly RemoteDeviceHandler RemoteDeviceHandler = new RemoteDeviceHandler();
+        static readonly ServiceHandler ServiceHandler = new ServiceHandler();
+
+        public ActionResult Index()
+        {
+            try
+            {
+                if (!ServiceHandler.IsStarted())
+                    throw new Exception("The Telldus service is not running.");
+
+                var test = RemoteDeviceHandler.GetRemoteDevices();
+
+                ViewBag.Devices = test;
+                return View();
+            }
+            catch (Exception exception)
+            {
+                ViewBag.ErrorMessage = exception.Message;
+                return View("Error");
+            }
+        }
+
+        public void TurnOn(int id)
+        {
+            RemoteDeviceHandler.TurnOn(id);
+        }
+
+        public void TurnOff(int id)
+        {
+            RemoteDeviceHandler.TurnOff(id);
+        }
+
+        public void TurnAllOn()
+        {
+            RemoteDeviceHandler.TurnAllOn();
+        }
+
+        public void TurnAllOff()
+        {
+            RemoteDeviceHandler.TurnAllOff();
+        }
+    }
+}
---
+++ Lights/Controllers/ServiceHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Web;
+
+namespace Lights.Controllers
+{
+    public class ServiceHandler
+    {
+        private readonly ServiceController _service;
+
+        public ServiceHandler()
+        {
+            _service = new ServiceController("Telldus Service");
+        }
+
+        public bool IsStarted()
+        {
+            _service.Refresh();
+            return _service.Status == ServiceControllerStatus.Running;
+        }
+
+        //public void StartService()
+        //{
+        //    if (_service.Status == ServiceControllerStatus.Running)
+        //        return;
+
+        //    _service.Start();
+        //    _service.WaitForStatus(ServiceControllerStatus.Running, new TimeSpan(0, 0, 30));
+        //}
+
+        //public void StopService()
+        //{
+        //    if (_service.Status == ServiceControllerStatus.Stopped)
+        //        return;
+
+        //    _service.Stop();
+        //    _service.WaitForStatus(ServiceControllerStatus.Stopped, new TimeSpan(0, 0, 30));
+        //}
+    }
+}
