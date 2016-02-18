a18adf45483364cef0da3dbdd50a052282337d3f
Mattias Sämskar <mattias.samskar@gmail.com>
2016-02-18 19:17:45 +0100

    Add Lights.Service

---
+++ Lights.Service/LightsService.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace Lights.Service
+{
+    public class LightsService : ServiceBase
+    {
+        public LightsService()
+        {
+            ServiceName = "LightsService";
+            CanStop = true;
+            CanPauseAndContinue = true;
+            AutoLog = true;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+
+        }
+    }
+}
