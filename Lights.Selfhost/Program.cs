0ef6d74797336d66d876d3802ce5cf85dba7331f
Mattias Sämskar <mattias.samskar@gmail.com>
2016-02-21 16:23:39 +0100

    Config Nancy to automatically create url reservations

--- Lights.Selfhost/Program.cs
+++ Lights.Selfhost/Program.cs
@@ -9,9 +9,10 @@ namespace Lights
         static void Main(string[] args)
         {
             LoadAssemblySoThatNancyFindsIt();
+
             var port = ConfigurationManager.AppSettings["port"];
 
-            using (var host = new NancyHost(new Uri("http://localhost:" + port)))
+            using (var host = new NancyHost(new HostConfiguration { UrlReservations = new UrlReservations { CreateAutomatically = true } }, new Uri("http://localhost:" + port)))
             {
                 Console.WriteLine("Host listening on port " + port);
                 host.Start();
